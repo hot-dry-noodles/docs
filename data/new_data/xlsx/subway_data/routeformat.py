7ca9674828b526a8d3e2a1f9de8486ce55534534
alios <aliosvictor@outlook.com>
2019-12-02 09:57:36 +0800

    Update route format

--- data/new_data/xlsx/subway_data/routeformat.py
+++ data/new_data/xlsx/subway_data/routeformat.py
@@ -6,17 +6,21 @@ filePath = 'distance_route_price.xlsx'
 wb = openpyxl.load_workbook(filePath)
 sheet = wb['sheet1']
 
+# for i in range(terminal_num * terminal_num):
+#     ori_route = str(sheet.cell(i + 1, 6).value)
+#     temp_route = ori_route[1:-1]
+#     terminal_line_list = temp_route.split(', ')
+#     current_route = ''
+#     for t in range(len(terminal_line_list) - 1):
+#         current_route += str(terminal_line_list[t])
+#         line = findLine(terminal_line_list[t], terminal_line_list[t + 1])
+#         current_route += str(line)
+#     current_route += str(terminal_line_list[-1])
+#     sheet.cell(i + 1, 6).value = current_route
+
 for i in range(terminal_num * terminal_num):
-    ori_route = str(sheet.cell(i + 1, 6).value)
-    temp_route = ori_route[1:-1]
-    terminal_line_list = temp_route.split(', ')
-    current_route = ''
-    for t in range(len(terminal_line_list) - 1):
-        current_route += str(terminal_line_list[t])
-        line = findLine(terminal_line_list[t], terminal_line_list[t + 1])
-        current_route += str(line)
-    current_route += str(terminal_line_list[-1])
-    sheet.cell(i + 1, 6).value = current_route
+    if sheet.cell(i + 1, 1).value == sheet.cell(i + 1, 3).value:
+        sheet.cell(i + 1, 6).value = sheet.cell(i + 1, 1).value
 
 print('finished!')
 wb.save(filePath)
