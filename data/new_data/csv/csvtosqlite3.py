7f1fc3bed5ee425f3bd4902c875a1f03f44ada28
alios <aliosvictor@outlook.com>
2019-12-16 17:59:25 +0800

    update_database

--- data/new_data/csv/csvtosqlite3.py
+++
@@ -1,6 +0,0 @@
-import pandas
-import csv, sqlite3
-conn= sqlite3.connect(r'E:\Projects\Git\hot-dry-noodles\data\new_data\csv\subwaydata.db3')
-df = pandas.read_csv(r'E:\Projects\Git\hot-dry-noodles\data\new_data\csv\distance_route_price.csv')
-df.to_sql('distance_route_price', conn, if_exists='append', index=False)
-print('ok')
