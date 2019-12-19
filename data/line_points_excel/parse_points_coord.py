daa4a6f9a5df61cddc02c81ea2b0fbcbfcc60dca
vtta <vtta0124@gmail.com>
2019-12-19 20:25:40 +0800

    Fix points shift

--- data/line_points_excel/parse_points_coord.py
+++ data/line_points_excel/parse_points_coord.py
@@ -3,8 +3,8 @@ import json
 from openpyxl import load_workbook
 
 wb_name = 'line_points.xlsx'
-wb = load_workbook(wb_name)
 
+wb = load_workbook(wb_name)
 for sheet_name in wb.sheetnames:
     sheet = wb[sheet_name]
     stations = {}
@@ -15,12 +15,12 @@ for sheet_name in wb.sheetnames:
         lng   = sheet['C{}'.format(row)].value
         lat   = sheet['D{}'.format(row)].value
         point['name'] = sname
-        point['lng'] = int(lng)
-        point['lat'] = int(lat)
+        point['lng'] = float(lng) * 1.5
+        point['lat'] = float(800 - lat) * 1.5
         stations[sid] = point
     # with open(sheet_name + '.json', 'w', encoding='utf8') as out:
     #     json.dump(stations, out, ensure_ascii=False)
     with open(sheet_name + '.txt', 'w', encoding='utf8') as out:
         for p in stations:
             out.write(json.dumps(stations[p], ensure_ascii=False))
-            out.write('\n')
+            out.write(',\n')
